6f66bd30fd0351d14ec886e9a4136864650b6005
Ivan Radojevic <ivan.radojevic@kraftwerk.co.at>
2023-06-06 14:34:45 +0200

    set font and container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from 'tailwindcss/defaultTheme'
+import forms from '@tailwindcss/forms'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,16 +7,22 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        './resources/js/**/*.vue'
     ],
 
     theme: {
+        container: {
+            center: true,
+            screens: {
+                lg: '1180px'
+            }
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans]
+            }
+        }
     },
 
-    plugins: [forms],
-};
+    plugins: [forms]
+}
