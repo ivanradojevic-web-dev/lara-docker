ad332561f6c6a13b40b868133039e0e134577e10
Ivan Radojevic <ivan.radojevic@kraftwerk.co.at>
2023-06-08 16:41:38 +0200

    Search Section

---
+++ resources/js/Components/SearchSection.vue
@@ -0,0 +1,82 @@
+<template>
+    <section class="mt-12 grid grid-cols-6 gap-x-4 gap-y-8">
+        <SearchList title="Artist" label="Genre">
+            <li
+                v-for="artist in results.artists"
+                :key="artist.artistId"
+                class="flex justify-between"
+            >
+                <Link href="#" class="text-2xl leading-none hover:text-red-500">
+                    {{ artist.artistName }}
+                </Link>
+                <span class="text-sm text-gray-500">
+                    {{ artist.primaryGenreName }}
+                </span>
+            </li>
+        </SearchList>
+
+        <SearchList title="Song" label="Artist">
+            <li
+                v-for="song in results.songs"
+                class="flex justify-between space-x-1"
+            >
+                <div class="flex space-x-2.5">
+                    <img
+                        :src="song.artworkUrl60"
+                        class="rounded-full w-6 h-6"
+                    />
+                    <span class="text-2xl leading-none">{{
+                        song.collectionName
+                    }}</span>
+                </div>
+                <div class="flex-none">
+                    <span class="text-sm text-gray-500">
+                        {{ song.artistName }}
+                    </span>
+                </div>
+            </li>
+        </SearchList>
+
+        <SearchList title="Album" label="Artist">
+            <li
+                v-for="album in results.albums"
+                class="flex justify-between space-x-1"
+            >
+                <div class="flex space-x-2.5">
+                    <img
+                        :src="album.artworkUrl60"
+                        class="rounded-full w-6 h-6"
+                    />
+                    <span class="text-2xl leading-none">{{
+                        album.collectionName
+                    }}</span>
+                </div>
+                <div class="flex-none">
+                    <span class="text-sm text-gray-500">
+                        {{ album.artistName }}
+                    </span>
+                </div>
+            </li>
+        </SearchList>
+
+        <SearchList title="Video" label="Name">
+            <li v-for="video in results.videos" class="flex justify-between">
+                <img :src="video.artworkUrl60" />
+                <span class="text-xs text-gray-500">
+                    {{ video.artistName }} - {{ video.trackName }}
+                </span>
+            </li>
+        </SearchList>
+    </section>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import SearchList from '@/Components/SearchList.vue'
+
+const props = defineProps({
+    results: {
+        type: Array
+    }
+})
+</script>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -22,81 +22,7 @@
                 </form>
             </section>
 
-            <section class="mt-12 grid grid-cols-6 gap-x-4 gap-y-8">
-                <SearchList title="Artist" label="Genre">
-                    <li
-                        v-for="artist in results.artists"
-                        :key="artist.artistId"
-                        class="flex justify-between"
-                    >
-                        <Link
-                            href="#"
-                            class="text-2xl leading-none hover:text-red-500"
-                        >
-                            {{ artist.artistName }}
-                        </Link>
-                        <span class="text-sm text-gray-500">
-                            {{ artist.primaryGenreName }}
-                        </span>
-                    </li>
-                </SearchList>
-
-                <SearchList title="Song" label="Artist">
-                    <li
-                        v-for="song in results.songs"
-                        class="flex justify-between space-x-1"
-                    >
-                        <div class="flex space-x-2.5">
-                            <img
-                                :src="song.artworkUrl60"
-                                class="rounded-full w-6 h-6"
-                            />
-                            <span class="text-2xl leading-none">{{
-                                song.collectionName
-                            }}</span>
-                        </div>
-                        <div class="flex-none">
-                            <span class="text-sm text-gray-500">
-                                {{ song.artistName }}
-                            </span>
-                        </div>
-                    </li>
-                </SearchList>
-
-                <SearchList title="Album" label="Artist">
-                    <li
-                        v-for="album in results.albums"
-                        class="flex justify-between space-x-1"
-                    >
-                        <div class="flex space-x-2.5">
-                            <img
-                                :src="album.artworkUrl60"
-                                class="rounded-full w-6 h-6"
-                            />
-                            <span class="text-2xl leading-none">{{
-                                album.collectionName
-                            }}</span>
-                        </div>
-                        <div class="flex-none">
-                            <span class="text-sm text-gray-500">
-                                {{ album.artistName }}
-                            </span>
-                        </div>
-                    </li>
-                </SearchList>
-
-                <SearchList title="Video" label="Name">
-                    <li
-                        v-for="video in results.videos"
-                        class="flex justify-between"
-                    >
-                        <img :src="video.artworkUrl60" />
-                        <span class="text-xs text-gray-500">
-                            {{ video.artistName }} - {{ video.trackName }}
-                        </span>
-                    </li>
-                </SearchList>
-            </section>
+            <SearchSection :results="results" />
         </MainLayout>
 
         <footer class="h-24"></footer>
@@ -106,10 +32,10 @@
 <script setup>
 import { ref, watch } from 'vue'
 import { debounce } from 'lodash'
-import { Head, Link, router } from '@inertiajs/vue3'
+import { Head, router } from '@inertiajs/vue3'
 import HeaderLayout from '@/Layouts/HeaderLayout.vue'
 import MainLayout from '@/Layouts/MainLayout.vue'
-import SearchList from '@/Components/SearchList.vue'
+import SearchSection from '@/Components/SearchSection.vue'
 
 defineProps({
     canLogin: {
